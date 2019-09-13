e4a6189886b450a604314cb2865aad9a03e11ca8
nanda <33324450+Bigblue00@users.noreply.github.com>
2019-09-13 12:06:41 +0530

    Update bin.py

--- week7/bin.py
+++ week7/bin.py
@@ -1,4 +1,11 @@
-n,m=map(int,input().split())
-for i in range(n):
-	for j in range(m):
-		a=list(map(int,input().split()))
+m,n=map(int,input().split())
+a=[list(map(int,input().split())) for i in range(m)]
+b='YES'
+for i in range(m):
+  for j in range(n):
+    if(a[i][j]!=0 and a[i][j]!=1):
+      b='NO'
+      break;
+  if(b=='NO'):
+    break
+print(b,end='')
